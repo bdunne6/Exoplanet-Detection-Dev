a5b4a2779498d5bc954bbc05ba1cca9fbbce8820
bdunne <brian.dunne@quartus.com>
2021-03-29 17:07:05 -0700

    Add script to visualize all release1 data.

---
+++ scripts/plot_release1_data.m
@@ -0,0 +1,23 @@
+load('release1_data.mat')
+
+d1 = release1_data;
+
+figure;
+psf1 = d1.cal.psf.data(:,:,1);
+imagesc(psf1)
+
+figure;
+tdata = d1.cal.transmission.data;
+plot(tdata(1,:),tdata(2,:));
+
+imgs = d1.images;
+figure;
+t = tiledlayout(5,6);
+t.TileSpacing = 'compact';
+t.Padding = 'compact';
+for i1 = 1:numel(imgs)
+    nexttile()
+    imagesc(imgs(i1).data);
+    [~,file_name,ext] = fileparts(imgs(i1).file_path);
+    title(file_name,'Interpreter','none')
+end
