a303112fa7320933661bb7550f2495863ebae4c9
bdunne <brian.dunne@quartus.com>
2021-04-08 09:25:11 -0700

    Add separate ParameterSet class to allow use with any arbitrary model rather than just ImageComponents.

---
+++ src/models/abstract_classes/ParameterSet.m
@@ -0,0 +1,35 @@
+classdef ParameterSet
+    %PARAMETERSET Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties (Abstract,Constant=true)
+       free_params 
+    end
+    
+    methods
+        function params_vect = vectorize_params(obj)
+            %concatenate all free properties into one vector for optimization
+            params_vect = [];
+            for i1 = 1:numel(obj)
+                free_params_i1 = obj(i1).free_params;
+                for i2 = 1:numel(free_params_i1)
+                    params_vect = [params_vect, obj(i1).(free_params_i1{i2})(:)'];
+                end
+            end
+        end
+        
+        function obj = devectorize_params(obj,params_vect)
+            %use the current object as a template to devectorize a param vector back into an obj
+            i_vect = 1;
+            for i1 = 1:numel(obj)
+                free_params_i1 = obj(i1).free_params;
+                for i2 = 1:numel(free_params_i1)
+                    Ni1 = numel(obj(i1).(free_params_i1{i2}));
+                    obj(i1).(free_params_i1{i2}) = params_vect(i_vect:i_vect+Ni1-1);
+                    i_vect = i_vect+Ni1;
+                end
+            end
+        end
+    end
+end
+
