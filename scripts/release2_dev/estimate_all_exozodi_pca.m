3feda785c03225292f5589859b28de0c244d2382
bdunne <brian.dunne@quartus.com>
2021-08-27 13:34:00 -0700

    Updates to PCA approach.

--- scripts/release2_dev/estimate_all_exozodi_pca.m
+++ scripts/release2_dev/estimate_all_exozodi_pca.m
@@ -10,24 +10,60 @@ load('img_set.mat');
 
 % s_select = struct('snr_level',3,'exozodi_intensity',2);
 % s_select = struct('snr_level',3);
-s_select = struct();
-[img_set, i_equal] = img_set.select('equal',s_select);
-img_set_new = img_set.stack_by({'passband'});
 
-X = zeros(numel(img_set_new.images),numel(img_set_new.images(1).data));
 
-img_size = size(img_set_new.images(1).data);
-for i1 = 1:numel(img_set_new.images)
-    X(i1,:) = img_set_new.images(i1).data(:);
+
+
+% s_select = struct('exozodi_model','rez','exozodi_intensity',3);
+% s_select = struct('exozodi_model','rez','exozodi_intensity',3);
+s_select = struct('exozodi_model','rez','exozodi_intensity',3);
+% s_select = struct('exozodi_intensity',2);
+% s_select = struct();
+[img_set_train1, i_equal] = img_set.select('equal',s_select);
+
+s_select = struct('exozodi_model','rez','exozodi_intensity',2);
+% s_select = struct('exozodi_intensity',2);
+% s_select = struct();
+[img_set_train2, i_equal] = img_set.select('equal',s_select);
+
+img_set_train = img_set;
+img_set_train.images = cat(2,img_set_train1.images,img_set_train2.images);
+
+%img_set_train = img_set_train2;
+
+% img_set_train = img_set;
+img_set_train = img_set_train.stack_by({'passband'});
+
+
+X = [];
+% rots = [0,1,2,3];
+rots = [0,2];
+
+for i0 = 1:numel(rots)
+    Xi0 = zeros(numel(img_set_train.images),numel(img_set_train.images(1).data));
+    img_size = size(img_set_train.images(1).data);
+    for i1 = 1:numel(img_set_train.images)
+        imgi1 = img_set_train.images(i1).data;
+        imgi1 = rot90(imgi1,rots(i0));
+        Xi0(i1,:) = imgi1(:);
+    end
+    X = cat(1,X,Xi0);
 end
 
-mdl = pca_model(X,0,1);
+
+% for i1 = 1:numel(img_set_train.images)
+%     imgi1 = img_set_train.images(i1).data;
+%     imgi1 =  rot90(imgi1,2);
+%     X(i1+numel(img_set_train.images),:) = imgi1(:);
+% end
+
+mdl = pca_model(X,1,0);
 
 
 
-figure;
-img_mean = reshape(mdl.Xm,img_size);
-imagesc(img_mean(:,:,2))
+% figure;
+% img_mean = reshape(mdl.Xm,img_size);
+% imagesc(img_mean(:,:,2))
 
 
 % hs = [0,1.0; %red saturated
@@ -47,49 +83,68 @@ imagesc(img_mean(:,:,2))
 
 
 figure(101)
+set(gcf,'Color','white')
 tile_size = [2,3];
 t1 = tiledlayout(tile_size(1),tile_size(2));
+t1.Padding = 'compact';
+t1.TileSpacing = 'compact';
 tile_2d = @(i,j) sub2ind(fliplr(tile_size),j,i);
 
-for i0 = 12:1:numel(img_set_new.images)
+s_select = struct('scenario',5,'exozodi_intensity',2,'snr_level',3,'visit_num',2);
+img_set_test = img_set.select('equal',s_select);
+img_set_test = img_set_test.stack_by({'passband'});
+
+vidObj = VideoWriter('pca1.mp4','MPEG-4');
+vidObj.FrameRate = 1;
+vidObj.Quality = 100;
+open(vidObj);
+for i0 = 1:numel(img_set_test.images)
     
-    
-    img_sample = img_set_new.images(i0).data;
+    img_sample = img_set_test.images(i0).data;
     scores = mdl.project(img_sample(:));
     
     %plot the original images
     nexttile(tile_2d(1,1))
     imagesc(img_sample(:,:,1))
-    title_str = img_set_new.images(i0).meta(1).file_name(8:end-5);
+    colorbar;
+    title_str = img_set_test.images(i0).meta(1).file_name(8:end-5);
     title(title_str,'Interpreter','none');
     
     nexttile(tile_2d(2,1))
     imagesc(img_sample(:,:,2))
-    title_str = img_set_new.images(i0).meta(1).file_name(8:end-5);
+    colorbar;
+    title_str = img_set_test.images(i0).meta(2).file_name(8:end-5);
     title(title_str,'Interpreter','none');
     
     %plot estimated background and residual
-    for i1 = 15%1:40%numel(scores)
+    for i1 = 1:50%1:40%numel(scores)
         reci1 = mdl.reconstruct(scores(1:i1));
         reci1 = reshape(reci1,img_size);
         
-        title_str = ['PCA projection for ' num2str(i1) ' components.'];
+        title_str = ['PCA model with ' num2str(i1) ' components.'];
         nexttile(tile_2d(1,2))
         imagesc(reci1(:,:,1))
+        colorbar;
         title(title_str,'Interpreter','none');
         
         nexttile(tile_2d(2,2))
         imagesc(reci1(:,:,2))
+        colorbar;
         title(title_str,'Interpreter','none');
-        drawnow();
         
         title_str  = 'residual';
         nexttile(tile_2d(1,3))
         imagesc(img_sample(:,:,1) - reci1(:,:,1))
+        colorbar;
+        title(title_str);
         nexttile(tile_2d(2,3))
         imagesc(img_sample(:,:,2) - reci1(:,:,2))
+        colorbar;
         title(title_str);
         drawnow();
-        pause(0.2);
+        pause(0.5);
+        currFrame = getframe(gcf);
+        writeVideo(vidObj,currFrame);
     end
 end
+close(vidObj);
--- src/models/pca/pca_model.m
+++ src/models/pca/pca_model.m
@@ -6,17 +6,20 @@ classdef pca_model
         X
         Xc
         Xm
+        Xstd
         N
         coeff
         scores
         latent
         mean_centered
+        normalized
     end
     
     methods
         function obj = pca_model(x,mean_center,normalize)
             if nargin > 0
                 obj.mean_centered = mean_center;
+                obj.normalized = normalize;
                 
                 obj.X = x;
                 obj.Xm = mean(x);
@@ -24,12 +27,26 @@ classdef pca_model
                 if mean_center
                     obj.Xc= obj.Xc-repmat(obj.Xm,size(x,1),1);
                 end
+                if normalize
+                    obj.Xstd = std(obj.Xc,1);
+                    obj.Xc= obj.Xc./obj.Xstd ;
+                end
+                
                 [obj.coeff, obj.scores, obj.latent] = pca_svd(obj.Xc);
             end
         end
         
         function [scores_out] = project(obj,x)
             %             scores_out = obj.coeff*x;
+                if obj.mean_centered
+                    x = x-obj.Xm(:);
+                end
+                if obj.normalized
+                    
+                    x= x./obj.Xstd(:);
+                end
+            
+            
             scores_out = x(:)'*obj.coeff;
         end
         function [x_out] = reconstruct(obj,scores_in)
@@ -37,9 +54,15 @@ classdef pca_model
             scores(1:length(scores_in)) = scores_in;
             x_out = (obj.coeff*scores(:))';
             
-            if obj.mean_centered;
+            if obj.normalized
+                x_out = x_out.*obj.Xstd;
+            end
+            
+            if obj.mean_centered
                 x_out = x_out +obj.Xm;
             end
+            
+            
         end
         
     end
