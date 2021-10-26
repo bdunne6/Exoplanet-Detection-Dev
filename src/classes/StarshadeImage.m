3ecef083218d11f6f4f7d6cc88166b99341cf50c
bdunne <brian.dunne@quartus.com>
2021-10-26 11:50:56 -0700

    Add an ROI feature such that a subset of the pixels can be accessed to reduce the dimensionality of the problem. Use a dependent property so original data size is preserved and the ROI is not a copy.

--- src/classes/StarshadeImage.m
+++ src/classes/StarshadeImage.m
@@ -1,33 +1,38 @@
 classdef StarshadeImage < matlab.mixin.Copyable
     %STARSHADEIMAGE Summary of this class goes here
     %   Detailed explanation goes here
-    
+
     properties
         meta
         data
+        roi %[x,y,w,h]
     end
-    
+
+    properties (Dependent)
+        data_roi
+    end
+
     methods
         function obj = StarshadeImage(varargin)
             %STARSHADEIMAGE Construct an instance of this class
             %   Detailed explanation goes here
-            
+
             %STARSHADEIMAGESET Construct an instance of this class
             %   Detailed explanation goes here
             p = inputParser;
             p.addOptional('file_path','',@ischar);
             p.addOptional('load_now',0);
-            
+
             p.parse(varargin{:});
             a = p.Results;
-            
+
             if ~isempty(a.file_path)
                 info = fitsinfo(a.file_path);
-                
+
                 [~,file_name,ext] =fileparts(a.file_path);
                 file_name= [file_name,ext];
                 name_parts = strsplit(file_name,'_');
-                
+
                 clear('meta');
                 %add image data and meta-data to a struct array
                 meta.file_name = file_name;
@@ -42,27 +47,51 @@ classdef StarshadeImage < matlab.mixin.Copyable
                 meta.passband = [str2double(name_parts{6}),str2double(name_parts{7})];
                 rparts = strsplit(name_parts{9},'.');
                 meta.release = str2double(rparts{1}(2:end));
-                
+
                 obj.meta = meta;
-                
+
                 if a.load_now
                     obj.load();
                 end
             end
         end
-        
+
         function img1 = load(obj)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
             img1 = [];
-            
+
             for i1 = 1:numel(obj.meta)
                 img1 = cat(3,img1,fitsread(obj.meta(i1).file_path,'raw'));
             end
-           
+
             if nargout == 0
                 obj.data = img1;
             end
         end
+
+
+        function val = get.data_roi(obj)
+            roi_1 = obj.roi;
+            if ~isempty(roi_1)
+                rows = roi_1(2):roi_1(2)+roi_1(4)-1;
+                cols = roi_1(1):roi_1(1)+roi_1(3)-1;
+                val = obj.data(rows,cols,:);
+            else
+                val = obj.data;
+            end
+        end
+
+        function set.data_roi(obj,val)
+            roi_1 = obj.roi;
+            if ~isempty(roi_1)
+                rows = roi_1(2):roi_1(2)+roi_1(4)-1;
+                cols = roi_1(1):roi_1(1)+roi_1(3)-1;
+                obj.data(rows,cols,:) = val;
+            else
+                obj.data = val;
+            end
+        end
+
     end
 end
--- src/classes/StarshadeImageSet.m
+++ src/classes/StarshadeImageSet.m
@@ -1,16 +1,24 @@
 classdef StarshadeImageSet < matlab.mixin.Copyable
     %STARSHADEIMAGESET Summary of this class goes here
     %   Detailed explanation goes here
-    
+
     properties
         instrument_meta
         images
     end
-    
+
+    properties (Dependent)
+        roi
+    end
+
+    properties (Access = private)
+        roi_
+    end
+
     properties (Access = private)
         default_ignore = {'fits_info','file_path','file_name'};
     end
-    
+
     methods
         function obj = StarshadeImageSet(varargin)
             %STARSHADEIMAGESET Construct an instance of this class
@@ -18,10 +26,10 @@ classdef StarshadeImageSet < matlab.mixin.Copyable
             p = inputParser;
             p.addOptional('image_folder','',@ischar);
             p.addOptional('load_now',0);
-            
+
             p.parse(varargin{:});
             a = p.Results;
-            
+
             if ~isempty(a.image_folder)
                 fits_files = dir(fullfile(a.image_folder,'*.fits'));
                 clear('images')
@@ -29,11 +37,11 @@ classdef StarshadeImageSet < matlab.mixin.Copyable
                     fits_pathi1 = fullfile(fits_files(i1).folder,fits_files(i1).name);
                     images(i1) = StarshadeImage(fits_pathi1,a.load_now);
                 end
-                
+
                 obj.images = images;
             end
         end
-        
+
         function [img_set_new,i_selected] = select(obj,varargin)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
@@ -41,103 +49,106 @@ classdef StarshadeImageSet < matlab.mixin.Copyable
             p.addParameter('equal',[]);
             p.addParameter('less_than',[]);
             p.addParameter('greater_than',[]);
-            
+
             p.parse(varargin{:});
             a = p.Results;
-            
+
             scomp = a.equal;
-            
+
             meta_vals = cat(1,obj.images.meta);
             meta_fields = fieldnames(meta_vals);
             cell_vals = struct2cell(meta_vals);
-            
+
             comp_fields = fieldnames(scomp);
             %cell_comp = struct2cell(scomp);
             %cellfun
-            
-            
+
+
             i_comp = ismember(meta_fields,comp_fields);
             cell_vals = cell_vals(i_comp,:);
             struct_vals = cell2struct(cell_vals,meta_fields(i_comp));
-            
-            
+
+
             i_selected = false(1,size(cell_vals,2));
             for i1 = 1:numel(struct_vals)
                 i_selected(i1) = isequal(scomp,struct_vals(i1));
             end
-            
+
             img_set_new = StarshadeImageSet();
             img_set_new.images = obj.images(i_selected);
             img_set_new.instrument_meta = obj.instrument_meta;
+            img_set_new.roi = obj.roi;
         end
-        
+
         function iu = unique(obj,comp_fields)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
-            
+
             meta_vals = cat(1,obj.images.meta);
             meta_fields = fieldnames(meta_vals);
             cell_vals = struct2cell(meta_vals);
-            
+
             i_comp = ismember(meta_fields,comp_fields);
             cell_vals = cell_vals(i_comp,:);
             struct_vals = cell2struct(cell_vals,meta_fields(i_comp));
-            
+
             hashes = cell(1,size(cell_vals,2));
             for i1 = 1:numel(struct_vals)
                 hashes{i1} = DataHash(struct_vals(i1));
             end
             [~,~,iu] = unique(hashes);
         end
-        
+
         function img_set_new = stack_by(obj,comp_fields)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
-            
+
             excluded_fields = [obj.default_ignore(:); comp_fields(:)];
             obj_fields = fieldnames(obj.images(1).meta(1));
             ufields = setdiff(obj_fields,excluded_fields);
-            
+
             u_indices = obj.unique(ufields);
-            
+
             u_ids = unique(u_indices);
             clear('images_new');
             for i1 = 1:numel(u_ids)
                 i_imagesi1 = u_ids(i1) == u_indices;
                 metai1 = cat(1,obj.images(i_imagesi1).meta);
                 datai1 = cat(3,obj.images(i_imagesi1).data);
-                
+
                 %create the array of output images
                 images_new(i1) = StarshadeImage();
                 images_new(i1).meta = metai1;
                 images_new(i1).data = datai1;
+                images_new(i1).roi = obj.roi;
             end
-            
+
             img_set_new = StarshadeImageSet();
             img_set_new.images = images_new;
             img_set_new.instrument_meta = obj.instrument_meta;
+            img_set_new.roi = obj.roi;
         end
-        
+
         function load(obj)
             for i1 = 1:numel(obj.images)
                 obj.images(i1).load();
             end
         end
-        
+
         function plot_all(obj,f1)
             n_images = numel(obj.images);
             tile_ratio_rc = 4/3;
             n_col = floor(sqrt(n_images*tile_ratio_rc));
-            
+
             n_row = ceil(n_images / n_col);
-            
+
             tile_size = [n_row,n_col];
             tile_2d = @(i,j) sub2ind(fliplr(tile_size),j,i);
-            
+
             figure(f1);
-            
+
             t1 = tiledlayout(tile_size(1),tile_size(2));
-            
+
             t1.TileSpacing = 'compact';
             for i1 = 1:numel(obj.images)
                 %imgi1 = obj.images.
@@ -154,10 +165,19 @@ classdef StarshadeImageSet < matlab.mixin.Copyable
                 imagesc(imgi1);
                 title(obj.images(i1).meta.file_name,'Interpreter','none');
             end
-            
+
         end
-        
-        
+
+        function set.roi(obj,val)
+            obj.roi_ = val;
+            for i1 = 1:numel(obj.images)
+                obj.images(i1).roi = val;
+            end
+        end
+        function val = get.roi(obj)
+             val = obj.roi_;
+        end
+
     end
 end
 
