507a5a07b9b7aacbfbc58aff63ba96c76505a402
bdunne <brian.dunne@quartus.com>
2022-04-26 11:23:50 -0700

    SNR dev script updates.

--- scripts/release2_dev/dev_snr1.m
+++ scripts/release2_dev/dev_snr1.m
@@ -1,10 +1,11 @@
 clear; setup_path();
+close all;
 
 
 mat_true = dir(fullfile('..','..','data','ground_truth','*.mat'));
 
 
-img_set = load(fullfile(mat_output_root,'img_set_disk_1em10_rev2_1.mat')).img_set;
+img_set = load(fullfile(mat_output_root,'img_set_disk_1em10_rev2.mat')).img_set;
 img_set = img_set.unstack;
 %img_set.
 
@@ -20,20 +21,24 @@ for i1 = 1:numel(mat_true)
         continue;
     end
 
-    cargs = image_m.meta.planet_locations.centroid_args;
+    for i2 = 1:numel(image_m.meta.planet_locations)
+    cargs = image_m.meta.planet_locations(i2).centroid_args;
 
-    w=7;
+
     %TODO: Fix the SNR calculation using proper PSF SNR estimation math:
     %https://www.stsci.edu/instruments/wfpc2/Wfpc2_hand6/ch6_exposuretime6.html
 
     [res1_0 ,cent0,w,sigma_lookup1] = cargs{:};
+        w=7;
 
     [cent1,cent_fit1] = refine_centroid_gaussian(res1_0 ,cent0,w,sigma_lookup1);
     cent_uncertainty = diff(cent_fit1.ci(2:3,:),1,2)/2;
     [~,SNR0,noise_mag,noise_per_pixel] = estimate_counts(res1_0,cent1,5);
 
-    %dev %%%%%%%%%%%%%%%%%%%%%%%%%
 
+    %dev %%%%%%%%%%%%%%%%%%%%%%%%%
+    PSF_fit = cent_fit1.y_opt - cent_fit1.x_opt(4);
+    %[SNR1] = estimate_SNR(image_m,PSF_fit,noise_per_pixel)
 
 
     int_times = image_m.lookup_fits_key('INTTIME');
@@ -45,7 +50,7 @@ for i1 = 1:numel(mat_true)
     readout_noise = readout_noise{1};
 
     detgain = image_m.lookup_fits_key('DETGAIN');
-    detgain = detgain{1};
+
 
     nframes = image_m.lookup_fits_key('NFRAMES');
     nframes = nframes{1};
@@ -62,16 +67,28 @@ for i1 = 1:numel(mat_true)
 
         int_time_s = int_time1
     %P_sky_background = noise_mag;
-    B = noise_per_pixel + eff_readout_noise^2;
+    %B = noise_per_pixel + eff_readout_noise^2;
 
-    S_e = S_fit
+    S_e = S_fit;
 
     SNR_num = S_e;
-    SNR_denom_sq = S_e + (( dark_curr*(int_time_s+46) + noise_per_pixel)/sharpness);
+    S_n = (( dark_curr*(int_time_s+46) + noise_per_pixel)/sharpness);
+    SNR_denom_sq = S_e + S_n;
 
     SNR1 = SNR_num/sqrt(SNR_denom_sq);
 
+    sqrt(dt.planets_total_PSF_counts)
     SNR1 
     SNR0
     dt.snr_goal
+
+    figure;
+    imagesc(res1_0);
+    hold on;
+    plot(cent0(1),cent0(2),'.r');
+
+    disp('GT ratios')
+    dt.planets_total_PSF_counts/(std(dt.scene_noise_realization(:))/sharpness)
+
+    end
 end
