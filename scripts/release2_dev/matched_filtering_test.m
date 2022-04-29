e651450159968fa00c6e0f715ae0dcc63e17f8a2
bdunne <brian.dunne@quartus.com>
2022-04-29 08:20:10 -0700

    Clean up matched filtering dev, and move mf detector function to dedicated file.

--- scripts/release2_dev/matched_filtering_test.m
+++ scripts/release2_dev/matched_filtering_test.m
@@ -8,16 +8,13 @@ rdist_max = 20;
 n_planets = 3;
 
 mat_true = dir(fullfile('..','..','data','ground_truth','*.mat'));
-dest_root = fullfile('..','mat_files');
+psf_mat = fullfile('..','mat_files','psf_data.mat');
 
-mat_out = fullfile(dest_root,'psf_data.mat');
-
-load(mat_out,'psf_data');
+load(psf_mat,'psf_data');
 
 load(fullfile(mat_output_root,'img_set_disk_1em10_rev2.mat'),'img_set');
 
 img_set = img_set.unstack();
-
 
 psf1 = psf_data(1).psfs(:,:,end);
 psf2 = psf_data(2).psfs(:,:,end);
@@ -121,22 +118,7 @@ for i1 = 1:numel(mat_true)
     hold on;
     plot(xt,yt,'.r')
     plot(xm,ym,'or')
-
-
-
-    %     figure;
-    %     subplot(1,2,1)
-    %     imagesc(imgf1)
-    %     hold on;
-    %     plot(xt,yt,'.r')
-    %     subplot(1,2,2)
-    %     imagesc(imgf2)
-    %         hold on;
-    %     plot(xt,yt,'.r')
-
 end
-
-
 
 
 it = ismember({cat(1,cat(1,img_set.images).meta).file_name},dt_lookup.keys);
@@ -145,9 +127,10 @@ img_set0 = img_set.copy();
 %img_set.images(~it) = [];
 dmax = 0;
 for i1 = 1:numel(img_set.images)
-
+    %% load image data %%%
     image_m = img_set.images(i1);
 
+    %% load appropriate PSF
     mlam = image_m.lookup_fits_key('MINLAM');
     mlam = mlam{1};
     if mlam == 425
@@ -156,34 +139,27 @@ for i1 = 1:numel(img_set.images)
         psf = psf2;
     end
 
+    %% use the ROI image
     img0 = image_m.data_roi;
 
-    %     bgnd1 = cat(3,image_m.meta(1).background_estimate,image_m.meta(2).background_estimate);
+    %% subtract previously computed background
     bgnd1 = image_m.meta(1).background_estimate;
-    sbgnd = img_set.images(i1).meta.disk.exozodi_image;
-    bgnd2 = crop_at_position(sbgnd,fliplr(size(sbgnd))/2,size(bgnd1));
+    %     sbgnd = img_set.images(i1).meta.disk.exozodi_image;
+    %     bgnd2 = crop_at_position(sbgnd,fliplr(size(sbgnd))/2,size(bgnd1));
 
     imgt = img0 - bgnd1;
 
+    %% do some additional background subtraction with a median filter
     img1 = imgt - medfilt2(imgt,[17,17]);
-    %img1 = img1;
 
+    %% run matched filtering based planet detector
+    pdet = mf_planet_detector(img1,psf,[rdist_min,rdist_max],n_planets);
 
-
-    %     figure;
-    %     imagesc(bgnd2)
-
-    %img1 = img0;
-
-    img_size = size(img1);
-    img_size = img_size(1:2);
-    img_c = fliplr(img_size)/2;
-
-
-    %imgf = convn(img1,psf,'same');
-
-    image_m.get_fits_keywords
-    img_mf = conv2(img1,psf,'same');
+    %% unpack outputs
+    img_mf = pdet.img_mf;
+    bin_lmax = pdet.bin_locmax;
+    g_ind = pdet.candidates.intensities;
+    cent_xy = pdet.detections.locations;
 
 
     if isstruct(image_m.meta.planets)&&isfield(image_m.meta.planets,'xy_pixels')
@@ -196,50 +172,14 @@ for i1 = 1:numel(img_set.images)
     end
     xym = [xm,ym];
 
-    [cent_xy,g_ind,bin_lmax] = planet_detection(img_mf,Inf);
 
-
-
-    r_dist = vecnorm(cent_xy - img_c ,2,2);
-    i_valid = (r_dist > rdist_min);
-    cent_xy = cent_xy(i_valid,:);
-    g_ind = g_ind(i_valid);
-    r_dist = r_dist(i_valid);
-
+    %remove detections that are close/redundant with previous detections
     [idx,d] = rangesearch(cent_xy,xym,2);
     i_rm = [idx{:}];
-    i_rm = [];
+    %i_rm = [];
     cent_xy(i_rm,:) = [];
     g_ind(i_rm) = [];
-    r_dist(i_rm) = [];
-
-    %i_outlier = isoutlier(g_ind,'gesd');
-    %i_outlier = isoutlier(g_ind,'median');
-    i_outlier = isoutlier(g_ind,'grubbs');
-    i_outlier = i_outlier&(g_ind > median(g_ind)); %positive outliers
-    i_outlier = i_outlier&(r_dist < rdist_max);%enforce max dist
-
-    i_outlier = find(i_outlier);
-    i_outlier = i_outlier(1:min(numel(i_outlier),n_planets));
-
-    g_ind_np = g_ind(i_outlier);
-
-    figure(103);
-    hold off;
-    histogram(g_ind)
-    hold on;
-    if ~isempty(g_ind_np)
-        plot(g_ind_np,0,'.','MarkerSize',15);
-    end
-
-    %     cent_xy = cent_xy(1:n_planets,:);
-    cent_xy = cent_xy( i_outlier,:);
-
-
-    %     img_lp = medfilt2(img1,[9 9]);
-    %     img_lp = medfilt2(img1,[9 9]);
-    %
-    %     img_lp = imbilatfilt(img1,1000);
+    %r_dist(i_rm) = [];
 
     s = 3;
     kg = fspecial('gaussian',round(s*[7 7]),s);
@@ -249,7 +189,7 @@ for i1 = 1:numel(img_set.images)
     img_c = fliplr(size(imgpt))/2;
 
 
-%     mad = median(abs(img_mf(:) - median(img_mf(:))));
+    %     mad = median(abs(img_mf(:) - median(img_mf(:))));
     mad = median(abs(g_ind(:) - median(g_ind(:))));
     th = mad*8;
 
@@ -266,7 +206,6 @@ for i1 = 1:numel(img_set.images)
         xt = nan; yt = nan;
     end
 
-    dt.exozodi_inclination_deg
 
     figure(101);
     set(gcf,'units','normalized','outerposition',[0 0 1 1])
@@ -282,64 +221,22 @@ for i1 = 1:numel(img_set.images)
     subplot(1,3,2)
     hold off;
     imagesc(img_mf)
-    % imagesc(img_lp)
     hold on;
     plot(xm,ym,'or')
     plot(xd,yd,'+r')
     plot(xt,yt,'.r')
-
 
 
     subplot(1,3,3)
     hold off;
-%     imagesc((img_mf>th)+(img_mf>mad*4))
     imagesc((img_mf>mad*3)&bin_lmax)
-    % imagesc(img_lp)
     hold on;
     plot(xm,ym,'or')
     plot(xd,yd,'+r')
     plot(xt,yt,'.r')
 
-
     dmax = max([dmax,vecnorm([xt(:),yt(:)]-img_c,2,2)']);
-    %         figure(102);
-    %     set(gcf,'units','normalized','outerposition',[0 0 1 1])
-    %         subplot(1,3,1)
-    %     hold off;
-    %     imagesc(img0);
-    %
-    %             subplot(1,3,2)
-    %     hold off;
-    %     imagesc(bgnd2);
-    %
-    %                 subplot(1,3,3)
-    %     hold off;
-    %     imagesc(img0- bgnd2);
 
-    %     figure;
-    %     subplot(1,2,1)
-    %     imagesc(imgf1)
-    %     hold on;
-    %     plot(xt,yt,'.r')
-    %     subplot(1,2,2)
-    %     imagesc(imgf2)
-    %         hold on;
-    %     plot(xt,yt,'.r')
     pause(1.5);
 end
 
-% function convolve_variable_psf(img1,psf_data)
-% img_c = fliplr(size(img1))/2;
-% img_c = img_c(1:2);
-%
-% img1_p =
-%
-%
-% [xg,yg] = meshgrid(1:size(img1,2),1:size(img1,1));
-% rdist = sqrt((xg-img_c(1)).^2 + (yg-img_c(2)).^2);
-%
-% for i1 = 1:size(img_c)
-%
-% sigma = interp1(sigma_lookup.pixel_dist,sigma_lookup.sigma,rdist,'nearest','extrap');
-%
-% end
---
+++ src/functions/mf_planet_detector.m
@@ -0,0 +1,54 @@
+function dout  = mf_planet_detector(img1,psf,r_range_pixels,n_max)
+
+img_size = size(img1);
+img_size = img_size(1:2);
+img_c = fliplr(img_size)/2;
+
+rdist_min = r_range_pixels(1);
+rdist_max = r_range_pixels(2);
+
+img_mf = conv2(img1,psf,'same');
+
+[cent_xy0,g_ind0,bin_lmax] = detect_local_maxima(img_mf,Inf);
+
+r_dist = vecnorm(cent_xy0 - img_c ,2,2);
+i_valid = (r_dist > rdist_min);
+cent_xy0 = cent_xy0(i_valid,:);
+g_ind0 = g_ind0(i_valid);
+r_dist = r_dist(i_valid);
+
+%     [idx,d] = rangesearch(cent_xy,xym,2);
+%     i_rm = [idx{:}];
+%     i_rm = [];
+%     cent_xy(i_rm,:) = [];
+%     g_ind(i_rm) = [];
+%     r_dist(i_rm) = [];
+
+%i_outlier = isoutlier(g_ind,'gesd');
+%i_outlier = isoutlier(g_ind,'median');
+i_outlier = isoutlier(g_ind0,'grubbs');
+i_outlier = i_outlier&(g_ind0 > median(g_ind0)); %positive outliers
+i_outlier = i_outlier&(r_dist < rdist_max);%enforce max dist
+
+i_outlier = find(i_outlier);
+i_outlier = i_outlier(1:min(numel(i_outlier),n_max));
+
+g_ind = g_ind0(i_outlier);
+cent_xy = cent_xy0( i_outlier,:);
+
+dout = struct();
+
+detections = struct();
+detections.locations = cent_xy;
+detections.intensities = g_ind;
+
+candidates = struct();
+candidates.locations = cent_xy0;
+candidates.intensities = g_ind0;
+candidates.i_detected = i_outlier;
+
+dout.img_mf = img_mf;
+dout.detections = detections;
+dout.candidates = candidates;
+dout.bin_locmax = bin_lmax;
+end
