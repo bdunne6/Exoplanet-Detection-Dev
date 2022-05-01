c500dae14d5f5b03d86fcc86d954447ef9ffe65a
bdunne <brian.dunne@quartus.com>
2022-04-30 22:28:24 -0700

    Add final rev4 jsons and script to load them.

--- scripts/results_generation/plot_results_summary.m
+++ scripts/results_generation/plot_results_summary.m
@@ -1,5 +1,11 @@
-json_file = 'level_1_results_rev2_1em10.json';
+json_file = 'level_1_results_rev4_1em10.json';
 fid = fopen(json_file,'r');
 json_string = fread(fid,'char=>char')';
 fclose(fid);
 data2 = jsondecode(json_string);
+
+json_file = 'level_1_results_rev4_1em9.json';
+fid = fopen(json_file,'r');
+json_string = fread(fid,'char=>char')';
+fclose(fid);
+data1 = jsondecode(json_string);
