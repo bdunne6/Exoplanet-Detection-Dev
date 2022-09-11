f696fb6d3390648114aa87da179b44d6174562fa
bdunne <brian.dunne@quartus.com>
2022-09-11 11:06:17 -0700

    Add SNR printout to ground truth for sanity check of updated SNR calculation.

--- scripts/results_generation/add_ground_truth.m
+++ scripts/results_generation/add_ground_truth.m
@@ -1,7 +1,9 @@
 mat_true = dir(fullfile('..','..','data','ground_truth','*.mat'));
 
+
+mat_root = mat_output_root();
 %load detections and estimates
-load(fullfile(mat_root,'img_set_disk_1em10_rev2.mat'));
+load(fullfile(mat_root,'img_set_disk_1em10_rev5.mat'));
 img_set = img_set.unstack();
 
 % [~,fname] = fileparts(mat_true(1).name);
@@ -22,7 +24,7 @@ for i1 = 1:numel(mat_true)
     [image_set_i1,i_selected] = img_set.select('equal',struct('file_name',fits_name));
 
     image_m = image_set_i1.images;
-    image_m.meta_true
+%     image_m.meta_true
 
 
     starflx = image_m.lookup_fits_key('STARFLX');
@@ -31,6 +33,8 @@ for i1 = 1:numel(mat_true)
     dm.star_total_PSF_counts = starflx{1}*inttime{1};
     dm.planets_total_PSF_counts = [];
     dm.planets_star_flux_ratio = [];
+    dm.snr_goal = [image_m.meta.planets.planet_SNR];
+
     if ~isempty(image_m.meta.planet_locations)
 
         for i2 = 1:numel(image_m.meta.planet_locations)
@@ -41,10 +45,16 @@ for i1 = 1:numel(mat_true)
 
     if ~isempty(dm.planets_star_flux_ratio)
         format long
-        disp('flux ratios:')
-        disp(dt.planets_star_flux_ratio)
-        disp(dm.planets_star_flux_ratio)
+%         disp('flux ratios:')
+%         disp(dt.planets_star_flux_ratio)
+%         disp(dm.planets_star_flux_ratio)
+
+        disp('SNR:')
+        disp(dm.snr_goal)
+        disp(dt.snr_goal)
+
     end
+    
 
 
 
